bec912a714c740d36a50bd59d3ab8469061446d5
Adeel <theadeelahmad07@gmail.com>
2022-04-25 13:41:23 -0700

    Minor bug fix

--- Events/guildCreate.js
+++ Events/guildCreate.js
@@ -3,6 +3,6 @@ const axios = require('axios')
 module.exports = {
     name: 'guildCreate',
     async execute(guild) {
-        await axios.post('http://127.0.0.1:8000/management/', {"id": guild.id});
+        await axios.post('http://127.0.0.1:8000/management/', {"id": guild.id}).catch(function (error) { return; });
     },
 };
